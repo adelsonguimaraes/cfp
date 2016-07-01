e3332ef6084b9c17581f36f19da69a451c00f865
Adelson Casa <adelsonguimaraes@gmail.com>
2016-07-01 00:48:39 -0400

    .

--- libs/js/controllers/controllers.js
+++ libs/js/controllers/controllers.js
@@ -199,8 +199,68 @@
 			$scope.novo = true;
 		}
 
+		$scope.listar = function () {
+			var data = {
+				"metodo":"listar",
+				"class":"recebimento"
+			};
+			genericAPI.generic(data)
+				.then(function successCallback(response) {
+					if(response.data.length>0){
+			        	$scope.recebimentos = response.data;
+			        }
+		        }, function errorCallback(response) {
+		        	//error
+				});	
+
+		}
+		$scope.listar();
+
+		$scope.editar = function (obj) {
+			obj.dataarrecadacao = moment(obj.dataarrecadacao).format('DD/MM/YYYY');
+			$scope.recebimento = obj;
+			$scope.novoRecebimento();
+		}
+
 		$scope.salvar = function (obj) {
-			console.log(obj);
+			
+			obj.idusuario = $rootScope.usuario.idusuario;
+			
+			var metodo = "cadastrar";
+			if(obj.id) metodo = "atualizar";
+
+			obj.dataarrecadacao = obj.dataarrecadacao.substr(6)+'-'+obj.dataarrecadacao.substr(3,2)+'-'+obj.dataarrecadacao.substr(0,2);
+			
+			var data = {
+				"metodo":metodo,
+				"data":obj,
+				"class":"recebimento"
+			};
+			genericAPI.generic(data)
+				.then(function successCallback(response) {
+					//success
+					startScope();
+					$scope.listar();
+		        }, function errorCallback(response) {
+		        	//error
+				});	
+		}
+
+		$scope.deletar = function (obj) {
+			
+			var data = {
+				"metodo":"deletar",
+				"data":obj,
+				"class":"recebimento"
+			};
+			genericAPI.generic(data)
+				.then(function successCallback(response) {
+					//success
+					startScope();
+					$scope.listar();
+		        }, function errorCallback(response) {
+		        	//error
+				});	
 		}
 
 		$scope.cancelar = function () {
